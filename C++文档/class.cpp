7f387505c495090dac0ad7b11f8db9c71a9329b8
ling797946 <119147681+ling797946@users.noreply.github.com>
2025-06-05 10:57:55 +0800

    Create class.cpp

---
+++ C++文档/class.cpp
@@ -0,0 +1,23 @@
+import std;
+
+using namespace std;
+
+class ai
+{
+	//	类成员列表
+public:
+	int a = 110;
+
+	int dd()
+	{
+		i.a;
+		return a;
+	}
+}i;	//	标记
+
+int main()
+{
+	ai ll;	//声明符列表
+
+	cout << ll.dd() << endl << i.a;
+}
