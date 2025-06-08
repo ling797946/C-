9196c4e4d67832135f5c40bd0b539355ddeb6f0b
ling797946 <119147681+ling797946@users.noreply.github.com>
2025-06-08 15:51:10 +0800

    Update class.cpp

--- C++文档/class.cpp
+++ C++文档/class.cpp
@@ -2,18 +2,21 @@ import std;
 
 using namespace std;
 
-class ai{
+class ai
+{
 	// 类成员列表
 public:
-	bool a {};
+	unsigned long long a =9999999999999999999999;
 
-	bool dd(){
+	unsigend long long dd()
+	{
 		i.a;
 		return i.a;
 	}
 }i;	// 标记
 
-int main(){
+int main()
+{
 	ai ll;	// 声明符列表
 
 	cout << ll.dd() << endl << i.a;
