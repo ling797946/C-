a230872c5eb82d4ad4dd3eb75762edc695ea18b8
ling797946 <119147681+ling797946@users.noreply.github.com>
2025-06-08 11:35:31 +0800

    Update ==!=.cpp

--- C++文档/==!=.cpp
+++ C++文档/==!=.cpp
@@ -2,18 +2,15 @@ import std;
 
 using namespace std;
 
-int main()
-{
+int main(){
 	bool a = true;
 	bool x = true;
 
-	if (a == x)
-	{
+	if (a == x){
 		cout << "南浪万岁";	// 执行
 	}
 
-	while (a != x)
-	{
+	while (a != x){
 		cout << "近前万岁";	// 不执行
 	}
 }
