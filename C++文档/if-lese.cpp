60387502a018401ceefc9275ceacdc966933106e
ling797946 <119147681+ling797946@users.noreply.github.com>
2025-06-08 16:04:29 +0800

    Update if-lese.cpp

--- C++文档/if-lese.cpp
+++ C++文档/if-lese.cpp
@@ -2,19 +2,23 @@ import std;
 
 using namespace std;
 
-int main(){
-	int a = 0;
+int main()
+{
+	long long a = 0;
 	
-	if (a > 1){
+	if (a > 1)
+	{
 		cout << "南风"; 	// 不执行
 	}
 
 	bool x = true;
 
-	if (x == true){
+	if (x == true)
+	{
 		cout << "近前无往";	// 执行
 	}
-	else{
+	else
+	{
 		cout << "秋风远到";	// 不执行
 	}
 }
