80faa83cdabb50120eb5d3ac34eaa7e695858797
ling797946 <119147681+ling797946@users.noreply.github.com>
2025-06-10 11:13:03 +0800

    Create protected.cpp

---
+++ C++文档/protected.cpp
@@ -0,0 +1,24 @@
+import std;
+
+using namespace std;
+
+class ai
+{
+public:
+	void ll()
+	{
+		cout << x << i;
+	}
+protected:
+	string x = "南浪近前情";
+	long long i = 21;
+};
+
+class aii :public ai{};
+
+int main()
+{
+	aii li;
+	
+	li.ll();
+}
