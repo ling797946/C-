0fa23c996e599b5d0d32e10f440293ab0b85c207
ling797946 <119147681+ling797946@users.noreply.github.com>
2025-06-21 10:17:09 +0800

    Update and rename class.cpp to clasds.cpp

--- C++文档/clasds.cpp
+++ C++文档/clasds.cpp
@@ -6,14 +6,14 @@ int main()
 {
 	if (true)	// 反之亦然，因为false会选择执行else中的语句或直接跳过。
 	{
-		cout << "近前";
+		cout << "近前";df 
 	}
 
 	int a = 1;
 
 	if (a > 0)	// 大于0即执行if中的语句，反正将执行else中的语句。
 	{
-		cout << "南风";
+		cout << "南风";df 
 	}
 	else
 	{
